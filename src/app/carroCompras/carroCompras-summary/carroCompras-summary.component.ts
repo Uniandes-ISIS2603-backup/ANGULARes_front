493ecc3eb17c56bd259d9c2adadeb4ded788ae4c
William Smith <wr.smith10@uniandes.edu.co>
2019-12-12 12:46:56 -0500

    carroCompras actualizado

---
+++ src/app/carroCompras/carroCompras-summary/carroCompras-summary.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+import { CartService } from "../carroCompras.service";
+@Component({
+selector: "carro-compras-summary",
+templateUrl:"carroCompras-summary.component.html"
+})
+export class CartSummaryComponent {
+constructor(public cartService: CartService) { }
+}
--- src/app/carroCompras/carroCompras.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from "@angular/core";
-import { CartService } from "./carroCompras.service";
-@Component({
-selector: "carro-compras",
-templateUrl:"carroCompras.component.html"
-})
-export class CartComponent {
-constructor(public cartService: CartService) { }
-}
